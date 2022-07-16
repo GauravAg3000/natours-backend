ae7c8d60ec41852c4f923cf6e60e42fed318de7c
Gaurav.Agarwal <gauravag3000@gmail.com>
2022-07-17 02:32:53 +0530

    added security middlewares

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -11,13 +16,45 @@ const app = express();
 
 console.log(process.env.NODE_ENV);
 
-// ATTACHING LOGGER MIDDLEWARE TO OUR APP
+// SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// ATTACHING LOGGER MIDDLEWARE
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// LIMITING REQUESTS FROM SAME IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. Please try again in an hour!",
+});
+
+app.use("/api", limiter);
+
 // BODY PARSER MIDDLEWARE IN EXPRESS
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS ATTACKS
+app.use(xss());
+
+// DATA SANITIZATION AGAINST PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "difficulty",
+      "maxGroupSize",
+      "price",
+    ],
+  })
+);
 
 // MIDDLEWARE FOR SERVING STATIC_FILES
 app.use(express.static(`${__dirname}/public`));
