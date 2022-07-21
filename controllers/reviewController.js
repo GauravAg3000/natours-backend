6ea49527206c6c7c29d5cf7cf93f98ff8dc8710d
Gaurav.Agarwal <gauravag3000@gmail.com>
2022-07-21 12:25:39 +0530

    added review handlers

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,24 +1,19 @@
 const Review = require("../models/reviewModel");
-const catchAsync = require("../utils/catchAsync");
-// const AppError = require("../utils/appError");
+const factory = require("./handlerFactory");
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+exports.setTourAndUserIds = (req, res, next) => {
+  //Allow Nested Routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user._id;
+  next();
+};
 
-  res.status(200).json({
-    status: "success",
-    results: reviews.length,
-    data: { reviews },
-  });
-});
+exports.getAllReviews = factory.getAll(Review);
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
+exports.getReview = factory.getOne(Review);
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      review: newReview,
-    },
-  });
-});
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
+
+exports.deleteReview = factory.deleteOne(Review);
