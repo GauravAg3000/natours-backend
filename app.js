6e5b51a8e5be1aba36c5f078df6319f865a9548e
Gaurav.Agarwal <gauravag3000@gmail.com>
2022-07-29 16:27:35 +0530

    Attached some middlewares

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -5,17 +6,25 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 // CREATING AN EXPRESS APPLICATION
 const app = express();
 
 console.log(process.env.NODE_ENV);
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// MIDDLEWARE FOR SERVING STATIC_FILES
+app.use(express.static(path.join(__dirname, "public")));
 
 // SET SECURITY HTTP HEADERS
 app.use(helmet());
@@ -36,6 +45,8 @@ app.use("/api", limiter);
 
 // BODY PARSER MIDDLEWARE IN EXPRESS
 app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
 
 // DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
 app.use(mongoSanitize());
@@ -57,16 +68,16 @@ app.use(
   })
 );
 
-// MIDDLEWARE FOR SERVING STATIC_FILES
-app.use(express.static(`${__dirname}/public`));
-
-// CUSTOM MIDDLEWARE
+// TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
   next();
 });
 
 // ROUTES MIDDLEWARE
+
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
