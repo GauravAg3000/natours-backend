f9257d9bba0862442bdc38f2d72efd3d3c86c619
Gaurav.Agarwal <gauravag3000@gmail.com>
2022-07-29 16:21:01 +0530

    viewscontroller for rendering pages

---
+++ controllers/viewsController.js
@@ -0,0 +1,67 @@
+const Tour = require("../models/tourModel");
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1. Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2. Build template
+  // 3. Render that template using tour data
+  res.status(200).render("overview", {
+    title: "All Tours",
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1.Get the data for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    select: "review rating user",
+  });
+
+  if (!tour) {
+    const err = new AppError(`There is no tour with that name`, 404);
+    return next(err);
+  }
+
+  // 2.Build Template
+  // 3.Render that template using data in <1>
+  res.status(200).render("tour", {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render("account", {
+    title: "Your account",
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render("account", {
+    title: "Your account",
+    user: updatedUser,
+  });
+});
